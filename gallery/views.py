d795c5e3dec4cd1ac90c82c4e4f4ced276827331
RKNyra <rk.nyra@gmail.com>
2019-11-10 15:34:52 +0300

    define a function that converts the date object to find the exact day of the week

--- gallery/views.py
+++ gallery/views.py
@@ -9,11 +9,25 @@ def home(request):
 
 def photos_of_day(request):
     date = dt.date.today()
+    
+    #function to convert date object to find the exact day
+    day = convert_dates(date)
     html = f'''
     <html>
         <body>
-            <h1>{date.day}-{date.month}-{date.year}</h1>
+            <h1>Photos for {day} {date.day}-{date.month}-{date.year}</h1>
         </body>
     </html>
     '''
     return HttpResponse(html)
+
+def convert_dates(dates):
+    #function that gets the weekday number for a given date
+    day_number = dt.date.weekday(dates)
+    
+    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    
+    #return the actual day of the week
+    day = days[day_number]
+    
+    return day
