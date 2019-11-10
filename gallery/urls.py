4a3541c1dab4d6a80a1d5e48f52034c81a5e880b
RKNyra <rk.nyra@gmail.com>
2019-11-10 13:57:15 +0300

    create dynamic content to display the current date photos

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('^$',views.home,name='welcome')
+    url('^$',views.home,name='home'),
+    url('^today/$',views.photos_of_day,name='photosToday')
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime as dt
 
 #views
 def home(request):
     return HttpResponse('Welcome to my photogallery')
+
+
+def photos_of_day(request):
+    date = dt.date.today()
+    html = f'''
+    <html>
+        <body>
+            <h1>{date.day}-{date.month}-{date.year}</h1>
+        </body>
+    </html>
+    '''
+    return HttpResponse(html)
