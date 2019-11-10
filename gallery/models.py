ce636016a0953ad023ed599c0badaac0e2e50916
RKNyra <rk.nyra@gmail.com>
2019-11-10 22:10:10 +0300

    create the Image, Location and Category models that will be used to define the image objects

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+import datetime as dt
 
-# Create your models here.
+
+#Category Model
+class Category(models.Model):
+    category = models.CharField(max_length =30)
+    
+    def __str__(self):
+        return self.category
+
+#Location Model
+class Location(models.Model):
+    location = models.CharField(max_length =70)
+    
+    def __str__(self):
+        return self.location
+
+#Image Model
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/', default='image.png')
+    name = models.CharField(max_length =70)
+    description = models.CharField(max_length =120)
+    post_date = models.DateTimeField(auto_now_add=True)
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+    
+    @classmethod
+    def today_photos(cls):
+        today = dt.date.today()
+        photos = cls.objects.filter(post_date__date = today)
+        return photos
+    
+    @classmethod
+    def days_photos(cls,date):
+        photos = cls.objects.filter(pub_date__date = date)
+        return photos
+    
+    def __str__(self):
+        return self.image
+    
+    def save_image(self):
+        self.save()
+    
+    class Meta:
+        ordering = ['post_date']
+    
--- photo_gallery/settings.py
+++ photo_gallery/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gallery.apps.GalleryConfig',
+    'gallery',
     'bootstrap4',
 ]
 
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'photo_gallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'photogallery',
+        'USER': 'rknyra',
+        'PASSWORD': 'rknyra7',
     }
 }
 
@@ -107,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
